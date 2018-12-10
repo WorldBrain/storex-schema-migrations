5a04caa8e098c4d06e02a04eee883679878d7775
Vincent den Boer <vincent@shishkabab.net>
2018-12-10 17:57:13 +0100

    Add failing unit test for schema-diff

---
+++ ts/schema-diff/index.test.ts
@@ -0,0 +1,55 @@
+import * as expect from 'expect'
+import StorageManager from 'storex';
+import { getStorageRegistryChanges } from '.'
+import { RegistryDiff } from './types';
+
+describe('Schema differ', () => {
+    it('should be able to diff to schemas', async () => {
+        const storageManager = new StorageManager({ backend: {configure: () => {}} as any })
+        storageManager.registry.registerCollections({
+            user: [
+                {
+                    version: new Date(2018, 7, 31),
+                    fields: {
+                        firstName: { type: 'string' },
+                        lastName: { type: 'string' },
+                    },
+                    indices: []
+                },
+                {
+                    version: new Date(2018, 8, 31),
+                    fields: {
+                        displayName: { type: 'string' },
+                    },
+                    indices: [
+                        { field: 'displayName' },
+                    ]
+                },
+            ],
+            foo: [{
+                version: new Date(2018, 8, 31),
+                fields: {
+                    blub: { type: 'string' },
+                },
+                indices: []
+            }],
+        })
+        await storageManager.finishInitialization()
+
+        const fromVersion = new Date(2018, 7, 31)
+        const toVersion = new Date(2018, 8, 31)
+        expect(getStorageRegistryChanges(storageManager.registry, fromVersion, toVersion)).toEqual({
+            fromVersion,
+            toVersion,
+            collections: {
+                created: ['foo'],
+                removed: [],
+                changed: {user: {
+                    fields: {created: ['displayName'], changed: {}, removed: ['firstName', 'lastName']},
+                    indices: {created: ['displayName'], removed: []},
+                    relationships: {created: [], removed: []}
+                }}
+            }
+        } as RegistryDiff)
+    })
+})
--- ts/schema-diff/index.ts
+++ ts/schema-diff/index.ts
@@ -1,7 +1,7 @@
 import StorageRegistry from 'storex/lib/registry'
 import { CollectionDefinition } from 'storex/lib/types'
 
-export function getStorageRegistryChanges(registry : StorageRegistry) {
+export function getStorageRegistryChanges(registry : StorageRegistry, fromVersion : Date, toVersion : Date) {
 
 }
 
